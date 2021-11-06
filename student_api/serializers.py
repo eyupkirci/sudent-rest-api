cb39de8cd21b013eefeeb28f0b3f78f9d0ecc2b8
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-11-06 13:57:38 +0300

    serializer fixed 2

--- student_api/serializers.py
+++ student_api/serializers.py
@@ -7,6 +7,13 @@ from rest_framework import serializers
 from .models import Student, Path
 
 class PathSerializer(serializers.ModelSerializer):
+    # students = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
+    # students = serializers.HyperlinkedRelatedField(
+    #     read_only=True,
+    #     many=True,
+    #     view_name='detail'
+    #     )
+    
     class Meta:
         model = Path
         fields ='__all__'
@@ -16,8 +23,14 @@ class PathSerializer(serializers.ModelSerializer):
 class StudentSerializer(serializers.ModelSerializer):
     path=serializers.StringRelatedField()
     path_id=serializers.IntegerField()
+    paths=serializers.SerializerMethodField()
     class Meta:
         model = Student
-        fields = ["id", "first_name", "last_name", "number", "path", "path_id"]
+        fields = ["id", "first_name", "last_name", "number", "path", "path_id", "paths"]
         # fields = '__all__'
         # exclude = ['number']
+    def get_paths(self,obj):
+        paths_data=Path.objects.all()
+        serializer = PathSerializer(paths_data, many=True)
+        
+        return serializer.data
